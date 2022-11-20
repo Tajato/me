95e14763e81609d044dfac55630895213a85d08a
Tajato <tahjgordon22@gmail.com>
2022-11-19 21:03:53 -0500

    third case study

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,16 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navBarLinks = document.getElementsByClassName('navbar-links')[0]
+const button = document.getElementsByClassName('resume')[0]
+const hoverLink = document.getElementsByClassName('resume-word')[0]
 
 toggleButton.addEventListener('click',() => {
     navBarLinks.classList.toggle('active')
+})
+
+button.addEventListener('mouseover', () => {
+    hoverLink.style.color = "white";
+})
+button.addEventListener('mouseout', () => {
+    
+    hoverLink.style.color="#3d115f";
 })
